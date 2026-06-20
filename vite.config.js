284571808bda01d5032f56fcf3d5372ef49000c1
vmvvisakh <visakhvijayan798@gmail.com>
2026-06-20 21:41:22 +0530

    chore: configure base path for production deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ESR-Map-App/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
